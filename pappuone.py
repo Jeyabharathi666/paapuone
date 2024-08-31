2f2ae54f63b7362d12a5268c3ca9683085eb0c93
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2024-08-31 22:03:26 +0530

    Update pappuone.py

--- pappuone.py
+++ pappuone.py
@@ -13,7 +13,7 @@ from concurrent.futures import ThreadPoolExecutor, wait
 Charting_Link = "https://chartink.com/screener/"
 Charting_url = 'https://chartink.com/screener/process'
 
-spreadsheet_name = "pappuone"
+spreadsheet_name = "paapuone"
 worksheet_letter = "p"
 total_no_of_worksheet = 29
 
