6d0076bb1146b508a601aaa4ea3e93bce9f55c8a
Jeyabharathi666 <saravananjeyabharathi@gmail.com>
2026-03-05 14:51:24 +0530

    Fix syntax error in worksheet_names list

--- paapuonep3.py
+++ paapuonep3.py
@@ -31,7 +31,7 @@ URLS = [
 ]
 
 sheet_id = "1h57GGy1883PE9MgqGg3oD7tyQqU3NxuTJwSwDnYOcZk"
-worksheet_names = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23""p24"]
+worksheet_names = ["p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16","p17","p18","p19","p20","p21","p22","p23","p24"]
 
 def scrape_chartink(url, worksheet_name):
     print(f"\n🚀 Starting scrape for '{worksheet_name}'")
